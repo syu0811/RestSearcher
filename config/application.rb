1071b4216f5a1f18bae85b82bb0c8bbebb5577eb
syu0811 <codamayu494949@gmail.com>
2022-01-28 22:36:46 +0900

    微調整

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,20 @@ module RestSearcher
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    # アプリケーションが対応している言語のホワイトリスト(ja = 日本語, en = 英語)
+    config.i18n.available_locales = [:ja, :en]
+
+    # 上記の対応言語以外の言語が指定された場合、エラーとするかの設定
+    config.i18n.enforce_available_locales = true
+
+    # デフォルトの言語設定
+    # config.i18n.default_locale = :en
+    config.i18n.default_locale = :ja
+
+    config.paths.add 'lib', eager_load: true
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -18,5 +32,15 @@ module RestSearcher
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
