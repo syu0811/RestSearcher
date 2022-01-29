54baccc68b66bbb4a8b6cb11a26da1438700a0c2
syu0811 <codamayu494949@gmail.com>
2022-01-29 20:57:37 +0900

    テストケースの追加（リクエストのみ）/#3

--- lib/api/gourmet_search.rb
+++ lib/api/gourmet_search.rb
@@ -1,7 +1,7 @@
 module Api
   class GourmetSearch
     def get_restaurants_hash(_param_url = "")
-      api_key = "e8cc8848cc539438"
+      # api_key = "e8cc8848cc539438"
       url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=e8cc8848cc539438&large_area=Z011"
       xml_to_hash(url)
     end
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe SearchController, type: :controller do
+  describe "/search" do
+    it "ステータス OK が返ってくる" do
+      get :index
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
