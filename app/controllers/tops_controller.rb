11b44ef98c3f20e630674958a050187246ed6726
syu0811 <codamayu494949@gmail.com>
2022-01-29 19:38:13 +0900

    緯度と経度の取得確認

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -3,14 +3,5 @@ class TopsController < ApplicationController
   def index
     gs = Api::GourmetSearch.new
     @hash = gs.get_restaurants_hash
-
-    
-    # client = HTTPClient.new                 # インスタンスを生成
-    # response = client.get(url)              # Getリクエスト
-    # @shops = REXML::Document.new(response.body)
-    # @datas = Hash.from_xml(response.body) 
-
   end
-
-  
 end
