3c71b8b4429b5aca133d003be3eabce0c609746b
syu0811 <codamayu494949@gmail.com>
2022-01-30 18:17:38 +0900

    検索結果ページの追加/#5

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -5,7 +5,7 @@ window.addEventListener('DOMContentLoaded', ()=>{
     if(typeof lat == "undefined" || typeof lng == "undefined") {
       document.getElementById( 'error' ).innerHTML = '<p>位置情報を取得してください</p>';
     } else {
-      let url = new URL(location);
+      let url = new URL("http://localhost:3000/results");
       // let url = new URL("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=e8cc8848cc539438");
       range = document.getElementById("range").value;
       url.searchParams.set('range', range);
