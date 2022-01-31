94bd999885f9e7166e3b1011a0c87ab56c3d790e
syu0811 <codamayu494949@gmail.com>
2022-01-31 13:31:50 +0900

    検索結果ページにデータとデザインを追加/#11

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,13 +1,13 @@
 class ResultsController < ApplicationController
   def index
-    @range = params[:range]
-    @lat = params[:lat]
-    @lng = params[:lng]
-    @page = get_page
+    range = params[:range]
+    lat = params[:lat]
+    lng = params[:lng]
+    @page = get_page < 1 ? 1 : get_page
 
     gs = Api::GourmetSearch.new
-    @hash = gs.get_restaurants_in_range(@range, @lat, @lng, @page)
-    next_hash = gs.get_restaurants_in_range(@range, @lat, @lng, @page + 1)
+    @shops = gs.get_restaurants_in_range(range, lat, lng, @page)
+    next_hash = gs.get_restaurants_in_range(range, lat, lng, @page + 1)
     @next = next_hash['results'].key?('shop')
   end
 
