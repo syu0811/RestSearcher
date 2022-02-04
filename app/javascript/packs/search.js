6acb4824fe6e7bb79f54523745a5bcaee58fbdcd
syu0811 <codamayu494949@gmail.com>
2022-02-04 16:02:27 +0900

    検索項目を追加し、検索結果に対応/#15

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -1,13 +1,27 @@
 window.search = function(){
   if(typeof lat == "undefined" || typeof lng == "undefined") {
-    document.getElementById( 'result' ).innerHTML = '<br><p>現在地の位置情報を取得してください。</p>';
+    document.getElementById( 'result' ).innerHTML = '<br><p>現在地の位置情報を取得してください。</p><p>現在地が取得されていないと検索できません。</p>';
   } else {
     let url = new URL("http://localhost:3000/results");
     // let url = new URL("http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=e8cc8848cc539438");
     range = document.getElementById("range").value;
+    genre = document.getElementById("genre").value;
+    smoking = document.getElementById("smoking");
+    parking = document.getElementById("parking");
+
     url.searchParams.set('range', range);
     url.searchParams.set('lat', lat);
     url.searchParams.set('lng', lng);
+
+    if(genre != "---"){
+      url.searchParams.set('genre', genre);
+    }
+    if(smoking.checked){
+      url.searchParams.set('smoking', 1);
+    }
+    if(parking.checked){
+      url.searchParams.set('parking', 1);
+    }
     location.href = url.toString();
     // alert(url.toString());
   }
